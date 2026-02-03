beb7e8333204ec367ff5b0efadcaae9a44f94758
HwangLongg <longlongokok2@gmail.com>
2026-02-03 19:39:02 +0700

    Update Bai5.java

--- Bai5.java
+++ Bai5.java
@@ -1,19 +1,8 @@
-import java.util.regex.*;
-
 public class Bai5 {
-    public static void main(String[] args) {
-        String log = "2024-05-20 | User: Long | Action: BORROW | BookID: BK123";
-
-        // Tạo các "cái bẫy" () để hứng dữ liệu
-        String regex = "(\\d{4}-\\d{2}-\\d{2}) \\| User: (\\w+) \\| Action: (\\w+) \\| BookID: (\\w+)";
-
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(log);
-
-        if (m.find()) {
-            System.out.println("Ngày: " + m.group(1)); // Cái bẫy 1
-            System.out.println("Tên: " + m.group(2));  // Cái bẫy 2
-            System.out.println("Hành động: " + m.group(3)); // Cái bẫy 3
-        }
+    public static void main(String[] args){
+        Book book = new Book("B001", "Sách số 1", "Trần Văn A", 2020, 150000);
+        System.out.printf("%-8s | %-30s | %-20s | %-12s | %-12s\n", "Mã sách", "Tên sách", "Tác giả", "Năm xuất bản", "Giá");
+        book.showBookInfo();
     }
+
 }
