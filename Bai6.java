3e8abbe54b3d21189f97bfeec0b77db69da2c7ef
HwangLongg <longlongokok2@gmail.com>
2026-02-03 19:39:37 +0700

    Update Bai6.java
    Removed profanity filtering logic and added user information display.

--- Bai6.java
+++ Bai6.java
@@ -1,22 +1,12 @@
 public class Bai6 {
     public static void main(String[] args) {
-        String danhGia = "Quyển sách này rất tệ, nội dung dở quá.";
-        String[] blacklist = {"tệ", "dở"};
+        User user = new User("US001", "Nguyễn Văn A", "nvanhh12345", "nva@gmail.com");
 
-        // Duyệt từng từ xấu trong danh sách đen
-        for (int i = 0; i < blacklist.length; i++) {
-            String tuXau = blacklist[i];
+        System.out.println("Thông tin người dùng:  ");
+        user.showUserInfo();
 
-            // Tạo chuỗi **** có độ dài bằng từ xấu
-            String sao = "";
-            for (int j = 0; j < tuXau.length(); j++) {
-                sao += "*";
-            }
-
-            // Thay thế từ xấu bằng sao
-            danhGia = danhGia.replace(tuXau, sao);
-        }
-
-        System.out.println("Kết quả: " + danhGia);
+        // set lại mật khẩu và email test phương thức set
+        user.setPassword("");
+        user.setEmail("nvagmail.123");
     }
 }
