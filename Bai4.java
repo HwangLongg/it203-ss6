2d8c198979a6889704b92d4056640eca1a989408
HwangLongg <longlongokok2@gmail.com>
2026-02-03 19:38:26 +0700

    Update Bai4.java
    Removed old code for card validation and added employee object instantiation and display.

--- Bai4.java
+++ Bai4.java
@@ -1,23 +1,25 @@
-import java.util.Scanner;
-
 public class Bai4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập mã thẻ: ");
-        String maThe = sc.nextLine();
 
-        // Regex: 2 chữ hoa + 4 số năm + 5 số ngẫu nhiên
-        String mauRegex = "^[A-Z]{2}\\d{4}\\d{5}$";
+    public static void main(String[] args){
+        // Đối tượng với contructor không tham số
+        Employee employee1 = new Employee();
+        employee1.employeeId = "EM001";
+        employee1.name = "Phạm Văn D";
+        employee1.salary = 15000000;
 
-        if (maThe.matches(mauRegex)) {
-            System.out.println("Mã thẻ hợp lệ!");
-        } else {
-            // Kiểm tra từng đoạn để báo lỗi cụ thể
-            if (!maThe.startsWith("TV")) {
-                System.out.println("Lỗi: Phải bắt đầu bằng TV");
-            } else {
-                System.out.println("Lỗi: Sai định dạng năm hoặc số ngẫu nhiên");
-            }
-        }
+        // Đối tượng với contructor gồm mã và tên
+        Employee employee2 = new Employee("EM002", "Nguyễn Văn B");
+
+        // Đối tượng với contructor Có đầy đủ mã, tên, lương
+        Employee employee3 = new Employee("EM003", "Trần Thị C", 20000000);
+
+        System.out.println("Nhân viên 1: ");
+        employee1.showEmployeeInfo();
+
+        System.out.println("Nhân viên 2: ");
+        employee2.showEmployeeInfo();
+
+        System.out.println("Nhân viên 3: ");
+        employee3.showEmployeeInfo();
     }
 }
