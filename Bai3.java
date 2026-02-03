8e2d5b9a03469be9cc2a8b578fb6c2262eb99e96
HwangLongg <longlongokok2@gmail.com>
2026-02-03 19:37:53 +0700

    Update Bai3.java

--- Bai3.java
+++ Bai3.java
@@ -1,19 +1,9 @@
 public class Bai3 {
-    public static void main(String[] args) {
-
-        StringBuilder nhatKy = new StringBuilder();
-
-        nhatKy.append("BÁO CÁO NGÀY: 2026-01-28\n");
-
-        String[] maSach = {"BK001", "BK002", "BK003"};
-        String[] ngayMuon = {"20/01", "21/01", "22/01"};
-
-        for (int i = 0; i < maSach.length; i++) {
-
-            nhatKy.append("Mã: ").append(maSach[i]);
-            nhatKy.append(" | Ngày: ").append(ngayMuon[i]).append("\n");
-        }
-
-        System.out.println(nhatKy.toString());
+    public static void main(String[] args){
+        Product product = new Product("P001", "Laptop DELL core intel", 15000000);
+        product.showProductInfo();
+        // set lại với giá không hợp lệ
+        product.setPrice(-20000000);
+        product.showProductInfo();
     }
 }
